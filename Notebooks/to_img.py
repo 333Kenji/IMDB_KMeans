fc71992c0f1558024b7b0c9ab21ba4c021738896
333Kenji <333kenji@gmail.com>
2022-06-20 22:58:38 -0700

    added autosave for images, working on saving tables as jpg

---
+++ Notebooks/to_img.py
@@ -0,0 +1,26 @@
+def to_img(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    """
+    Summary: loads .tsv files in chunks, selecting specific features and filtering unnecessary values.
+        _
+    Args:
+        path (string): local address
+        cols (list): features to keep
+    Returns:
+       df_result (pd.DataFrame): preprocessed dataframe
+    """
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in mpl_table._cells.items():
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax.get_figure()
