44eaf16a1d38bcf1735d42c62c9724675f632b55
333Kenji <333kenji@gmail.com>
2022-06-21 16:27:44 -0700

    to_img set to be imported as a helper module

--- Notebooks/to_img.py
+++ Notebooks/to_img.py
@@ -1,22 +1,17 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+
 def to_img(data, col_width=4.0, row_height=0.625, font_size=14,
                      header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
                      bbox=[0, 0, 1, 1], header_columns=0,
                      ax=None, **kwargs):
-    """
-    Summary: loads .tsv files in chunks, selecting specific features and filtering unnecessary values.
-        _
-    Args:
-        path (string): local address
-        cols (list): features to keep
-    Returns:
-       df_result (pd.DataFrame): preprocessed dataframe
-    """
+    
     if ax is None:
-        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        size = (numpy.array(data.shape[::-1]) + numpy.array([0, 1])) * numpy.array([col_width, row_height])
         fig, ax = plt.subplots(figsize=size)
         ax.axis('off')
     mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
-    mpl_table.auto_set_font_size(False)
+    mpl_table.auto_set_font_size(True)
     mpl_table.set_fontsize(font_size)
 
     for k, cell in mpl_table._cells.items():
