cf438086b87c1f5a5fc9c95b07f81ec6344c1ab6
333Kenji <333kenji@gmail.com>
2022-06-21 18:51:29 -0700

    testing image uploads

--- Notebooks/to_img.py
+++ Notebooks/to_img.py
@@ -1,7 +1,7 @@
 import numpy as numpy
 import matplotlib.pyplot as plt
 
-def to_img(data, col_width=4.0, row_height=0.625, font_size=14,
+def to_img(data, name, col_width=4.0, row_height=0.625, font_size=10,
                      header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
                      bbox=[0, 0, 1, 1], header_columns=0,
                      ax=None, **kwargs):
@@ -21,4 +21,7 @@ def to_img(data, col_width=4.0, row_height=0.625, font_size=14,
             cell.set_facecolor(header_color)
         else:
             cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
-    return ax.get_figure()
+    fig = ax.get_figure()
+    fig.savefig(f'../Images/{name}.jpg')
+    plt.close()
+    return
